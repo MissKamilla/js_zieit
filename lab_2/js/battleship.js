8ed79f7caaec97bfb0f71150bdd79a96c7f48b74
MissKamilla <mis.kamillka@gmail.com>
2020-12-14 23:36:36 +0200

    js_zieit
    js_zieit

---
+++ lab_2/js/battleship.js
@@ -0,0 +1,58 @@
+﻿// Задание 1
+
+/*time = prompt("Введите в текстовое поле время в часах (от 0 до 23)");
+if (time <= 17 && time >= 0) {
+    alert("Добрый день");
+} else if(time > 17 && time <24){
+    alert("Добрый вечер");
+} else{
+    alert("Введите в текстовое поле время в часах (от 0 до 23)");
+}*/
+
+
+// Задание 2
+
+/*var name = prompt("Введите в текстовое поле имя");
+if (name == "Jonh") {
+    alert("Привет Jonh");
+} else{
+    alert("Я вас не знаю");
+}*/
+
+
+// Задание 3
+
+ /*var userName = prompt("Введите в текстовое поле имя");
+ var userPwd = prompt("Введите в текстовое поле пароль");
+ if ((userName=='ivan' && userPwd==333)|| (userName=='ssss' && userPwd==666) || (userName=='gibs' && userPwd==0000)) {
+     alert("Доброе пожаловать");
+ } else{
+     alert("Пользователь не найден");
+ }*/
+
+
+// Задание 4
+
+/*var num1 = numbs(num1);
+var num2 = numbs(num2);
+var num3 = numbs(num3);
+var max;
+function numbs(num) {
+    num = prompt("Введите в текстовое поле числo");
+    return num;
+}
+if(num1>num2){
+    if(num1>num3) max = "Max числo - "+num1;
+}
+if(num2>num1){
+    if(num2>num3) max = "Max числo - "+num2;
+}
+if(num3>num2){
+    if(num3>num2) max = "Max числo - "+num3;
+}
+if(num1==num2) max = "Два числа имеют max значение - "+num1;
+if(num1==num3) max = "Два числа имеют max значение - "+num2;
+if(num2==num3) max = "Два числа имеют max значение - "+num3;
+if(num1==num2==num3) max = "Все числа имеют max значение - "+num3;
+
+alert(max);*/
